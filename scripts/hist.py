afca68cf682705e7775079af57ce9870a8b29c41
ivankp <ivan.pogrebnyak@gmail.com>
2018-09-11 00:31:19 -0400

    updated scripts/hist.py

--- scripts/hist.py
+++ scripts/hist.py
@@ -5,32 +5,44 @@ import sys, json, lzma
 argc = len(sys.argv)
 data = None
 
+if argc<2:
+    print("usage: {} hists.json[.xz] select.json".format(sys.argv[0]))
+    sys.exit(0)
+
 with lzma.open(sys.argv[1]) if sys.argv[1].endswith('.xz') \
 else open(sys.argv[1]) as f:
     data = json.load(f)
 
-hists = data['histograms']
-bins_fmt = data['annotation']['bins']
+hists      = data['histograms']
+categories = data['annotation']['bins'][:-1]
+weights    = data['annotation']['weights']
 
-if argc==2:
+if argc<3:
     print('\033[34;1mHistograms:\033[0m')
     for hist_name in hists:
         print(hist_name)
-    print('\n\033[34;1mBin values:\033[0m')
-    for x in bins_fmt:
+    print('\n\033[34;1mCategories:\033[0m')
+    for x in categories:
         print('{}: {}'.format(x[0],', '.join(x[1])))
+    print('\n\033[34;1mWeights:\033[0m')
+    for x in weights:
+        print(x)
     sys.exit(0)
 
-ii = [ bins_fmt[i][1].index(x) for i,x in enumerate(sys.argv[3].split(':')) ] \
-     if argc>3 else [ ]
+select = None
+with open(sys.argv[2]) as f:
+    select = json.load(f)
 
-hist = hists[sys.argv[2]]
+hist = hists[select["hist"]]
+ci   = [ c[1].index(select[c[0]]) for c in categories ]
+wi   = weights.index(select["weight"])
+
 bins = [ ]
 for b in hist["bins"]:
-    for i in ii:
+    for i in ci:
         if b is None: break
         b = b[i]
-    bins.append(b)
+    bins.append([ b[0][wi], b[1] ])
 
-print(json.dumps({ 'axes': hist['axes'], 'bins': bins }, separators=(',',':')))
+print(json.dumps({ 'axes': hist['axes'], 'bins': bins },separators=(',',':')))
 
