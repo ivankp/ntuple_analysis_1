a6a4f261d953a4ea0d90d335d4e2c97bd639a111
ivankp <ivan.pogrebnyak@gmail.com>
2018-10-06 23:47:04 -0400

    working on hist reader

--- src/read_hist.cc
+++ src/read_hist.cc
@@ -3,6 +3,8 @@
 #include <nlohmann/json.hpp>
 #include "ivanp/scribe.hh"
 #include "ivanp/functional.hh"
+#include "ivanp/error.hh"
+#include "ivanp/string.hh"
 
 #define TEST(var) \
   std::cout << "\033[36m" #var "\033[0m = " << var << std::endl;
@@ -11,30 +13,65 @@ using std::cout;
 using std::endl;
 
 using ivanp::scribe::size_type;
+using namespace ivanp;
 
 int main(int argc, char* argv[]) {
   ivanp::scribe::reader sr(argv[1]);
   TEST(sr.head())
-  TEST(((const void*)sr.head().data()))
+  // TEST(((const void*)sr.head().data()))
   auto head = nlohmann::json::parse(sr.head());
   nlohmann::json sel;
   std::ifstream(argv[2]) >> sel;
 
-  TEST(sel["hist"])
-  TEST(sr.ptr())
-  auto hist = sr[sel["hist"]];
-  TEST(hist.type_name())
-  const auto type = hist.get_type();
-  TEST(type.name())
   /*
-  ivanp::y_combinator([](auto f, const auto t, auto name, int indent=0) -> void {
-    for (int i=indent; i; --i) cout << "  ";
-    cout << t.name();
-    if (!name.empty()) cout << " : " << name;
-    cout << endl;
-    for (auto t : t) f(t.type,t.name,indent+1);
-  })(type,std::string{});
+  auto hist = sr[sel["hist"]];
+  TEST(hist.ptr())
+  TEST(hist.type_name())
+  auto bins = hist["bins"];
+  TEST(bins.ptr())
+  TEST(bins.type_name())
+  TEST(bins.size())
+  // for (const auto& bin : bins) {
+  //   TEST(bin.type_name())
+  //   TEST((unsigned)bin.union_index())
+  // }
+  TEST(bins[1].type_name())
+  TEST((unsigned)bins[1].union_index())
+  TEST(bins[1][0].type_name())
+  TEST(bins[1][0][0].type_name())
+  TEST(bins[1][0][0]["n"].type_name())
+  TEST(bins[1]["all"]["all"]["w"].type_name())
+  TEST(bins[4]["all"]["all"]["w"][0][0].cast<double>())
   */
+
+  /*
+  for (auto bin : sr[sel.at("hist")]["bins"]) {
+    if (bin.union_index()==0) continue;
+    bin = *bin;
+    for (;;) {
+      const char* type_name = bin.type_name();
+      TEST(type_name)
+      if (starts_with(type_name,"nlo_bin<")) break;
+      bin = bin[sel.at(type_name)];
+    }
+  }
+  */
+
+  y_combinator([&](auto f, const auto& bins) -> void {
+    for (auto bin : bins) {
+      const auto ui = bin.union_index();
+      if (ui==0) continue;
+      bin = *bin;
+      if (ui==2) f(bin);
+      for (;;) {
+        const char* type_name = bin.type_name();
+        TEST(type_name)
+        if (starts_with(type_name,"nlo_bin<")) break;
+        bin = bin[sel.at(type_name)];
+      }
+    }
+  })( sr[sel.at("hist")]["bins"] );
+
 
   /*
   TEST(hist.ptr())
